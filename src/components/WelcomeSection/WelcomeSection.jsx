b5628bc9d00b2affa649774f79bc32fc1c28e476
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-07-20 00:06:06 +0200

    added Welcomesection, fixing css

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,3 +1,23 @@
-const WelcomeSection = () => {};
+import { NavLink } from 'react-router-dom';
+import css from './WelcomSection.module.css';
+
+
+const WelcomeSection = () => {
+     return (
+    <div className={css.welcomeSection}>
+      <p className={css.text}>Record daily water intake and track</p>
+         <h1 className={css.mainTitle}>
+          <span>Water</span> consumption tracker</h1>
+      <div className={css.linkThumb}>
+        <NavLink to="/signup" className={css.linkTracker}>
+          Try tracker
+        </NavLink>
+        <NavLink to="/signin" className={css.linkSignIn}>
+          Sign In
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
 export default WelcomeSection;
