814bcd996712ee8afdf2d1dac47df59cdf6f42a3
IrynaLebedenko <94801312+IrynaLebedenko@users.noreply.github.com>
2024-07-20 17:36:34 +0300

    create modal windows and setting form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,20 @@
-const App = () => {};
+import DeleteWaterModal from '../WaterModal/waterDeleteModal/DeleteWaterModal';
+import LogOutModal from "../LogOutModal/LogOutModal";
+import UserSettingForm from '../UserSettingsForm/UserSettingsForm';
+import { useState } from 'react';
+
+
+const App = () => {
+    const [modalState, setModalState] = useState(false);
+    const handleSubmit = (data) => {
+    console.log(data);
+  };
+     return <div className="App">
+        <DeleteWaterModal call={modalState} onClose={() => setModalState(false)}/>
+        <LogOutModal call={modalState} onClose={() => setModalState(false)} /> 
+        <UserSettingForm onSubmit={handleSubmit} />
+    </div> 
+
+};
 
 export default App;
