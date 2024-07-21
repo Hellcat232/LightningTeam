6a9e43b0503f7429b6ab8c414a094cfe09edc6b2
Olena Kryvenko <sashakryvenko2006@gmail.com>
2024-07-21 11:01:02 +0200

    add show/hide password button in signIn and signUp sections

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
+import { FaRegEyeSlash } from "react-icons/fa6";
 
 const SignInSchema = yup.object().shape({
   email: yup.string().email("Invalid email").required("Email is required"),
@@ -19,6 +21,12 @@ const SignInForm = () => {
   } = useForm({
     resolver: yupResolver(SignInSchema),
   });
+
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordShown(!passwordShown);
+  };
 
   const onSubmit = (data) => {
     dispatch(login(data));
@@ -40,10 +48,12 @@ const SignInForm = () => {
         <label>password</label>
         <input
           name="password"
-          type="password"
+          type={passwordShown ? "text" : "password"}
           placeholder="Enter your password"
           {...register("password")}
         />
+        <FaRegEyeSlash onClick={togglePasswordVisibility} />
+
         {errors.password && <p>{errors.password.message}</p>}
 
         <button type="submit">Sign In</button>
