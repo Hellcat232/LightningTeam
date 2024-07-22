fe9641faaa61b3a6b0550f0b54d011763b680605
spaceTony9 <bilokon.fw7@gmail.com>
2024-07-22 13:28:04 +0200

    Fixed Settings modal

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -18,11 +18,6 @@ const UserBar = ({ togglePopover, isPopoverOpen }) => {
       ) : (
         <img src={avatarPlaceholder} className={css.userImg} />
       )}
-      {/*<img*/}
-      {/*  className={css.userImg}*/}
-      {/*  src="https://thumbs.dreamstime.com/b/people-person-fun-joy-funny-facial-expression-concept-close-up-phoyo-portrait-charming-glad-gorgeous-nice-cute-lovely-lady-272795823.jpg"*/}
-      {/*  alt="userPhoto"*/}
-      {/*/>*/}
       {isPopoverOpen ? (
         <svg className={css.icon}>
           <use href={`${spriteHref}#icon-hide_burger`}></use>
