97ab28455af811c5817cd959cc4e1a3f45ff9127
Hellcat232 <maxkuich1993@gmail.com>
2024-07-27 16:57:46 +0300

    fix progressBar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,12 +1,16 @@
-import css from './ProgressBar.module.css';
+import css from "./ProgressBar.module.css";
 
 const ProgressBar = ({ percentage }) => {
+  if (percentage > 100) {
+    percentage = 100;
+  }
+
   return (
     <div className={css.progressBar}>
       <div className={css.progress} style={{ width: `${percentage}%` }}>
         <div className={css.progressIndicator}></div>
       </div>
-      {percentage === '30' || percentage === '70' ? (
+      {percentage === "30" || percentage === "70" ? (
         <div
           className={`${css.milestone}`}
           style={{ left: `${percentage - 3}%` }}
