1c2bf4a26e18ad42d05e9592e83326a4b0788e94
Timoshchyk <timosuko202@gmail.com>
2024-07-27 12:56:25 +0300

    css-welconSec

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
 import WelcomeSection from "../../components/WelcomeSection/WelcomeSection.jsx";
-import { Page } from "../../components/Page/Page";
 import styles from "./HomePage.module.css";
 import React from "react";
 
@@ -8,7 +7,6 @@ const HomePage = () => {
   return (
     <div className={styles.HomePageSection}>
       <title>Home Page</title>
-      <Page/>
       <WelcomeSection />
       <AdvantagesSection />
     </div>
